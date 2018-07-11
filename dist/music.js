2998f570823f88834f56b2b9d1ed16743534a61e
candy <1601796593@qq.com>
2018-07-11 13:03:36 +0800

    Site updated: 2018-07-11 13:03:17

--- dist/music.js
+++ dist/music.js
@@ -4,27 +4,22 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
       {
-        name: "PDD洪荒之力",
-        artist: '徐梦圆',
-        url: 'http://up.mcyt.net/?down/39868.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
+        name: "一百万个可能",
+        artist: 'Christine Welch',
+        url: 'http://sc1.111ttt.cn/2016/5/01/14/194140050065.mp3',
+        cover: 'http://imge.kugou.com/stdmusic/20150715/20150715222012914700.jpg',
       },
       {
-        name: '9420',
-        artist: '麦小兜',
-        url: 'http://up.mcyt.net/?down/45967.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/8.jpg',
+        name: 'All Right',
+        artist: '金艺林',
+        url: 'https://f1.w.hjfile.cn/doc/201306/all%20right020665424.mp3',
+        cover: 'http://imge.kugou.com/stdmusic/20130625/20130625203104423771.jpg',
       },
       {
-        name: '风筝误',
-        artist: '刘珂矣',
-        url: 'http://up.mcyt.net/?down/46644.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+        name: '懂得又如何',
+        artist: '金艺林',
+        url: 'https://f1.w.hjfile.cn/doc/201306/dongdeyouruhe020676161.mp3',
+        cover: 'http://imge.kugou.com/stdmusic/20150720/20150720104058467780.jpg',
       }
     ]
 });
-
-作者：恬雅过客
-链接：https://www.jianshu.com/p/93341c0aacbf
-來源：简书
-简书著作权归作者所有，任何形式的转载都请联系作者获得授权并注明出处。
