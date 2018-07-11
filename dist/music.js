170a8035217bd8c8aa1ebed7abf6ba2f5ae131f5
candy <1601796593@qq.com>
2018-07-11 12:51:30 +0800

    Site updated: 2018-07-11 12:51:20

---
+++ dist/music.js
@@ -0,0 +1,30 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "PDD洪荒之力",
+        artist: '徐梦圆',
+        url: 'http://up.mcyt.net/?down/39868.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
+      },
+      {
+        name: '9420',
+        artist: '麦小兜',
+        url: 'http://up.mcyt.net/?down/45967.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/8.jpg',
+      },
+      {
+        name: '风筝误',
+        artist: '刘珂矣',
+        url: 'http://up.mcyt.net/?down/46644.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+      }
+    ]
+});
+
+作者：恬雅过客
+链接：https://www.jianshu.com/p/93341c0aacbf
+來源：简书
+简书著作权归作者所有，任何形式的转载都请联系作者获得授权并注明出处。
