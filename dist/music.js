1de83d79f8da5f2e2e2a2e7ddcc7b3c98ea42fb8
candy <1601796593@qq.com>
2018-07-12 14:44:34 +0800

    Site updated: 2018-07-12 14:44:10

--- dist/music.js
+++ dist/music.js
@@ -4,12 +4,6 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
       {
-        name: "一百万个可能",
-        artist: 'Christine Welch',
-        url: 'http://sc1.111ttt.cn/2016/5/01/14/194140050065.mp3',
-        cover: 'http://imge.kugou.com/stdmusic/20150715/20150715222012914700.jpg',
-      },
-      {
         name: 'All Right',
         artist: '金艺林',
         url: 'https://f1.w.hjfile.cn/doc/201306/all%20right020665424.mp3',
